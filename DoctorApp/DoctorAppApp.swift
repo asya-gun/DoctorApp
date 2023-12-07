e5e3dc64efd23c8b23806db03e8683aad56ccbbe
Asya Checkanar <asyach3ck@gmail.com>
2023-12-07 14:40:27 +0300

    Initial Commit

---
+++ DoctorApp/DoctorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorAppApp.swift
+//  DoctorApp
+//
+//  Created by Asya Checkanar on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DoctorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
